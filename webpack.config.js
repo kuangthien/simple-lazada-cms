2a2bce063c01ee41d7bd4510b8cc760911df9d89
Tran Kuang Thien <kuangthien@gmail.com>
2019-03-04 00:51:02 +0700

    config build package

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
-const path = require('path');
+const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-	entry: './src/cms/cms.js',
-	output: {
-		filename: './cms.js',
-		path: path.resolve(__dirname, 'dist'),
-	},
-	devServer: {
-		contentBase: './dist',
-	},
-};
+    entry: './src/cms/cms.js',
+    output: {
+        filename: './cms.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        contentBase: './dist',
+    },
+    plugins: [new CopyPlugin([{ from: './public/images', to: './images' }])],
+}

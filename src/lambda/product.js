bf5df6b7635a9a8a2c3aca61d66a0e3dd6671dd8
Tran Kuang Thien <kuangthien@gmail.com>
2019-03-04 00:07:49 +0700

    api get product detail

---
+++ src/lambda/product.js
@@ -0,0 +1,78 @@
+const axios = require('axios')
+const yamlFront = require('yaml-front-matter')
+
+// serverless letlify lambda function,
+// intermediate between client and github apiv4
+
+const URL = `https://api.github.com/graphql`
+const accessToken = '555b08f7398de19f0d03d026c2d7791fe4ab4596 '
+
+const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Methods': '*',
+    'Access-Control-Max-Age': '2592000',
+    'Access-Control-Allow-Credentials': 'true',
+}
+
+const getProductDetailData = fileName => {
+    const query = `
+    query {
+        repository(name: "simple-lazada-cms", owner: "kuangthien") {
+            object(expression: "master:content/products/${fileName}") {
+                ... on Blob {
+                    text
+                }
+            }
+        }
+    }`
+    console.log(fileName)
+    return axios({
+        method: 'POST',
+        url: URL,
+        data: JSON.stringify({ query }),
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    })
+        .then(res => yamlFront.loadFront(res.data.data.repository.object.text))
+        .catch(e => e)
+}
+const getResponseBody = async path => {
+    rs = await getProductDetailData(path + '.md')
+
+    return {
+        _id: path,
+        imageUrl: rs.productFeaturedImage,
+        title: rs.title,
+        desc: rs.__content,
+        prices: {
+            regular: rs['price-regular'],
+            old: rs['price-old'],
+        },
+    }
+}
+
+exports.handler = async (event, context, callback) => {
+    // Make sure method is GET
+    try {
+        // Run
+        const body = await getResponseBody(event.path.replace('/product/', ''))
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(body, null, 4),
+            isBase64Encoded: false,
+            headers: {
+                ...headers,
+            },
+        }
+    } catch (e) {
+        console.log(e) // output to netlify function log
+        return {
+            statusCode: 500,
+            body: e.message, // Could be a custom message or object i.e. JSON.stringify(err)
+        }
+    }
+}
